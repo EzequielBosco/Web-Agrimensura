f94471cc464269b6b364ac350fdc6775e6f40e28
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-10-04 16:13:36 -0300

    vercel almacenamiento

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-khh^a$*kd8g&dmw7pknk(l3!sqe8&lne)t17%$64-ff9$p^82r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -72,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
+# WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
 
 # Database
