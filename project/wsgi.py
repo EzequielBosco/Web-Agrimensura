e536da64eefb1875b4ee746b5ed1520a2c05e730
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-10-11 12:19:37 -0300

    production vercel

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
